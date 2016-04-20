2a2a81ffc2807b80559bdae5840a9b7529c8fd3f
tty-tourist <andreasrasholm@gmail.com>
2016-04-20 15:45:36 +0200

    more cleanup

--- org-tracktable.el
+++ org-tracktable.el
@@ -161,26 +161,10 @@ If `org-tracktable-daily-goal' is set to more than 0, show % of daily goal."
                                (round (* 100 (/ (org-tracktable-written-today)
                                                 (float org-tracktable-daily-goal)))))))))
 ;;;###autoload
-(defun org-tracktable-status-buffer ()
-  "Reports number of words in the buffer."
-  (interactive)
-   (let ((wc (org-tracktable-word-count (point-min) (point-max))))
-     (message (format "%d words in buffer." wc))))
-
-;;;###autoload
 (defun org-tracktable-status-today ()
   "Reports number of words written today"
   (interactive)
   (message "%s" (format "%d words written today." (org-tracktable-written-today))))
-
-;;;###autoload
-(defun org-tracktable-status-today-percent ()
-  "Reports number of words written today"
-  (interactive)
-  (message "%s" (format "%d words written today. %d%% of daily goal"
-                        (org-tracktable-written-today)
-                        (round (* 100 (/ (org-tracktable-written-today)
-                                                (float org-tracktable-daily-goal)))))))
 
 ;;;###autoload
 (defun org-tracktable-write ()
